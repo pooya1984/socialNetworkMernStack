7153f07b96dd7a2f61188f05c879a011a86e4f1a
pooya <pooya.nodehi@gmail.com>
2019-09-30 16:20:40 +0200

    initial init

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const connectDB = require("./config/db");
+const app = express();
+
+//connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+app.get("/", (req, res) => res.send("API Runing"));
+
+//Define Routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/auth", require("./routes/api/auth"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`server started on port ${PORT}`));
